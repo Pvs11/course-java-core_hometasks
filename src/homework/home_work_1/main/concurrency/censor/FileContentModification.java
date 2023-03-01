bc037be4360c350166c7f3e257e0ddf0b64bfb1b
Pavel Shahoika <pavel11sg@gmail.com>
2023-03-01 06:30:50 +0300

    concurrency

---
+++ src/homework/home_work_1/main/concurrency/censor/FileContentModification.java
@@ -0,0 +1,43 @@
+package homework.home_work_1.main.concurrency.censor;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileContentModification implements Runnable {
+    FileQueue fileQueue;
+    File wordsFile;
+    public FileContentModification(FileQueue fileQueue, File file) {
+        this.fileQueue = fileQueue;
+        this.wordsFile = file;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s has started word's removing\n", Thread.currentThread().getName());
+        List<String> wordsToBeRemoved = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(wordsFile))) {
+            wordsToBeRemoved = br.lines().flatMap(l -> Arrays.stream(l.split("\s"))).toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File changedFile = new File("src/homework.home_work_1.main.concurrency.main/concurrency/" + fileQueue.resultFile.getName().replace(".txt", "Changed.txt"));
+        try (BufferedReader br = new BufferedReader(new FileReader(fileQueue.resultFile));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(changedFile))) {
+            changedFile.createNewFile();
+            List<String> finalWordsToBeRemoved = wordsToBeRemoved;
+            bw.write(br.lines().map(str -> {
+                for (String s : finalWordsToBeRemoved) {
+                    if (str.contains(s)) {
+                        str = str.replaceAll(s, "");
+                    }
+                }
+                return str + "\n";
+            }).collect(Collectors.joining()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("%s finished refactoring\n", Thread.currentThread().getName());
+    }
+}
---
+++ src/homework/home_work_1/main/concurrency/exception/UserInputException.java
@@ -0,0 +1,7 @@
+package homework.home_work_1.main.concurrency.exception;
+
+public class UserInputException extends Exception {
+    public UserInputException(String msg) {
+        super(msg);
+    }
+}
